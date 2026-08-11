aa1223dfbf867742480e1cc0f19aafd60b93d672
assumcaonerd <assumcao@gmail.com>
2026-08-11 10:12:31 -0300

    Add generative backend: update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,5 @@ import globals from 'globals';
 export default [
   { ignores: ['dist/**'] },
   { files: ['src/**/*.js'], languageOptions: { ecmaVersion: 'latest', sourceType: 'module', globals: globals.browser }, rules: { ...js.configs.recommended.rules, 'no-unused-vars': ['error', { argsIgnorePattern: '^_' }] } }
+  ,{ files: ['api/**/*.js', 'server.js', 'tests/**/*.js'], languageOptions: { ecmaVersion: 'latest', sourceType: 'module', globals: globals.node }, rules: { ...js.configs.recommended.rules, 'no-unused-vars': ['error', { argsIgnorePattern: '^_' }] } }
 ];
